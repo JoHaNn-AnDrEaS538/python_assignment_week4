4218000930afcb395c5228f926471aa3261f2471
Johann ANdreas <68011990@kmitl.ac.th>
2026-07-20 12:27:20 +0700

    sorting assignments

---
+++ assignment5/utilities/calculator.py
@@ -0,0 +1,18 @@
+#Assignment2
+
+def add(x,y):
+    sum1=x+y
+    print("Sum of numbers = ",sum1)
+
+def subtract(x,y):
+    diff = x-y
+    print("difference of two numbers = ",diff)
+
+def multiply(x,y):
+    product = x*y
+    print("product of the numbers = ",product)
+
+def divide(x,y):
+    quotient = x/y
+    print("quotient of the numbers = ",quotient)
+

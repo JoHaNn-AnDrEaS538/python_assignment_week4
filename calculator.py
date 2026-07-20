696e96139e699422e45ade0b0c8348653fa8757f
Johann ANdreas <68011990@kmitl.ac.th>
2026-07-20 11:38:07 +0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,22 @@
+#Assignment2
+
+def add(x,y):
+    sum1=x+y
+    print("Sum of numbers = ",sum1)
+
+def subtract(x,y):
+    diff = x-y
+    print("difference of two numbers = ",diff)
+
+def multiply(x,y):
+    product = x*y
+    print("product of the numbers = ",product)
+
+def divide(x,y):
+    quotient = x/y
+    print("quotient of the numbers = ",quotient)
+
+add(3,2)
+subtract(3,2)
+multiply(3,2)
+divide(3,2)

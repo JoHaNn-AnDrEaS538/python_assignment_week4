a29f760f46083dabd8563889f52cfcf7d00790a6
Johann ANdreas <68011990@kmitl.ac.th>
2026-07-20 12:17:16 +0700

    importing and using modules

---
+++ main.py
@@ -0,0 +1,15 @@
+#Assignment4
+
+import string_operations
+
+sample_string = "advance programming lab assignment"
+
+print("Original:", sample_string)
+print("Reversed:")
+string_operations.reverse_string(sample_string)
+print("Capitalized:")
+string_operations.capitalize_string(sample_string)
+print("Lowercase:")
+string_operations.lowercase_string(sample_string)
+print("Uppercase:")
+string_operations.uppercase_string(sample_string)
---
+++ string_operations.py
@@ -0,0 +1,16 @@
+#module for strings
+def reverse_string(str1):
+    reverse = str1[::-1]
+    print(reverse)
+
+def capitalize_string(str1):
+    caps=str1.capitalize()
+    print(caps)
+
+def lowercase_string(str1):
+    lower=str1.lower()
+    print(lower)
+
+def uppercase_string(str1):
+    upper=str1.upper()
+    print(upper)

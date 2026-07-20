b53e8d1b7ed93a29f1bdfe7f78c4f0a4b40293ea
Johann ANdreas <68011990@kmitl.ac.th>
2026-07-20 11:50:51 +0700

    Create Lambda_calcultor.py

---
+++ Lambda_calcultor.py
@@ -0,0 +1,15 @@
+#Assignment3
+
+add =lambda x, y: x+y
+print("Sum of numbers =", add(3,2))
+
+subtract =lambda x, y:x-y
+print("Difference of the numbers =", subtract(3,2))
+
+multiply =lambda x, y: x*y
+print("Product of two numbers =", multiply(3,2))
+
+divide = lambda x,y: x/y
+print("Quotient of the numbers =", divide(3,2))
+
+
